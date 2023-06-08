e958a6b1d268ed02c024c57e0f9b39357ac97f8f
Jackson Price <price.jacksonk@gmail.com>
2023-06-08 16:57:56 -0500

    Add: initial dog scopes and testing

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+  scope :any_size, -> { where('min_weight > 0 AND max_weight < 250') }
+  scope :small, -> { where("max_weight < 22") }
+  scope :medium, -> { where('min_weight > 22 AND max_weight < 55') }
+  scope :large, -> { where('min_weight > 55 AND max_weight < 115') }
+  scope :very_large, -> { where('max_weight > 115') }
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:small_dog) { Dog.create(min_weight: 10, max_weight: 15) }
+  let!(:medium_dog) { Dog.create(min_weight: 25, max_weight: 35) }
+  let!(:large_dog) { Dog.create(min_weight: 85, max_weight: 112) }
+  let!(:very_large_dog) { Dog.create(min_weight: 115, max_weight: 245) }
+
+  after(:each) do
+    Dog.delete_all
+  end
+
+  describe '.any_size' do
+    it 'returns dogs of any size' do
+      result = Dog.any_size
+      expect(result).to include(small_dog, medium_dog, large_dog, very_large_dog)
+    end
+  end
+
+  describe '.small' do
+    it 'returns small dogs' do
+      result = Dog.small
+      expect(result).to include(small_dog)
+      expect(result).not_to include(medium_dog, large_dog, very_large_dog)
+    end
+  end
+
+  describe '.medium' do
+    it 'returns medium dogs' do
+      result = Dog.medium
+      expect(result).to include(medium_dog)
+      expect(result).not_to include(small_dog, large_dog, very_large_dog)
+    end
+  end
+
+  describe '.medium' do
+    it 'returns medium dogs' do
+      result = Dog.medium
+      expect(result).to include(medium_dog)
+      expect(result).not_to include(small_dog, large_dog, very_large_dog)
+    end
+  end
+
+  describe '.large' do
+    it 'returns large dogs' do
+      result = Dog.large
+      expect(result).to include(large_dog)
+      expect(result).not_to include(small_dog, medium_dog, very_large_dog)
+    end
+  end
+
+  describe '.very_large' do
+    it 'returns very large dogs' do
+      result = Dog.very_large
+      expect(result).to include(very_large_dog)
+      expect(result).not_to include(small_dog, medium_dog, large_dog)
+    end
+  end
 end
