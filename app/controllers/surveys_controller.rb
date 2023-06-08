8ed0039a1adcc6caa85ddefcf156cb43ae5ec7ca
Jackson Price <price.jacksonk@gmail.com>
2023-06-07 21:34:19 -0500

    Add: create survey functionality

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,12 +1,25 @@
 class SurveysController < ApplicationController
   def new
+    @survey = Survey.new
+    @questions = Question.all.sort_by { |question| question.order }
   end
 
   def create
+    @survey = Survey.new(survey_params)
+    respond_to do |format|
+      if @survey.save
+        format.html { redirect_to new_survey_path, notice: "Survey was successfully created." }
+      else
+        Rails.logger.info @survey.errors.messages
+      end
+    end
   end
 
   private
 
   def survey_params
+    params.require(:survey).permit(:name_input, :size_input, :family_input, :children_input, :other_dog_input, :shedding_input, :grooming_input,
+                                   :drooling_input, :stranger_input, :playfulness_input, :protective_input, :adaptability_input,
+                                   :trainability_input, :energy_input, :barking_input, :mental_stim_input)
   end
 end
