1ab099fc327767a54d35643e5e15c3156d9c4289
Jackson Price <price.jacksonk@gmail.com>
2023-06-09 14:15:58 -0500

    Survey results view changes

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -1,6 +1,7 @@
 class SurveyResultsController < ApplicationController
   def show
     @survey_result = SurveyResult.find params[:id]
+    @survey = Survey.find @survey_result.survey_id
     @suggested_dog_breed = Dog.find @survey_result.dog_breed_id
   end
 end
