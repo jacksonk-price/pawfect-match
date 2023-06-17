1fa52fbb841e2ff3a87bb3b5854116bf6bf25b77
Jackson Price <price.jacksonk@gmail.com>
2023-06-16 22:42:12 -0500

    Add: styling and attribute display for top 3

--- app/helpers/survey_results_helper.rb
+++ app/helpers/survey_results_helper.rb
@@ -1,2 +1,19 @@
 module SurveyResultsHelper
+  def top_three_wording(index)
+    if index == 0
+      'Top match'
+    elsif index == 1
+      'Second best match'
+    else
+      'Third best match'
+    end
+  end
+
+  def display_score(score, index)
+    if score < index
+      '<i class="fa-solid fa-paw fa-2x gray-paw"></i>'
+    else
+      '<i class="fa-solid fa-paw fa-2x blue-paw"></i>'
+    end
+  end
 end
