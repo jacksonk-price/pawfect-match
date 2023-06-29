2a5a9fa3ee95d5b7553965764dd166f4d6ed9088
Jackson Price <price.jacksonk@gmail.com>
2023-06-28 19:56:56 -0500

    Add: front end validations

--- app/javascript/controllers/survey_controller.js
+++ app/javascript/controllers/survey_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['question'];
+    static targets = ['question', 'flash'];
     static classes = ['hidden'];
     initialize() {
         this.currentIndex = 0;
+        this.boolFlashShowing = false;
         this.hideQuestions();
         this.showCurrentQuestion();
     }
@@ -20,8 +21,43 @@ export default class extends Controller {
         currentQuestion.classList.remove(this.hiddenClass);
     }
 
+    validateCurrentQuestion() {
+        const currentQuestion = this.questionTargets[this.currentIndex];
+
+        if (this.currentIndex === 0) {
+            const input= currentQuestion.querySelector('input[type="text"]');
+            if (input.value.trim() === '') {
+                return false;
+            }
+        } else {
+            const selectedOption= currentQuestion.querySelector('input[type="radio"]:checked');
+            if (!selectedOption) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    showFlashError() {
+        this.flashTarget.classList.remove(this.hiddenClass);
+    }
+
+    hideFlashError() {
+        this.flashTarget.classList.add(this.hiddenClass);
+    }
+
     goToNextQuestion() {
+        if (!this.validateCurrentQuestion()) {
+            this.showFlashError();
+            this.boolFlashShowing = true;
+            return;
+        }
+
         if (this.currentIndex < this.questionTargets.length - 1) {
+            if(this.boolFlashShowing) {
+                this.hideFlashError();
+            }
             this.currentIndex++;
             this.hideQuestions();
             this.showCurrentQuestion();
